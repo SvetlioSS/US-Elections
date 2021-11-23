837d15cadf1b17dbb6e8848cd02ee3d2dd10ec0a
Svetoslav Spasov <svetoslav.spasov@high5games.com>
2021-11-23 14:35:38 +0200

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("deploy-testnets", "Deploys contract on a provided network")
+  .setAction(async () => {
+    const deployElectionContract = require("./scripts/deploy");
+    await deployElectionContract();
+  });
+
+subtask("print", "Prints a message")
+  .addParam("message", "The message to print")
+  .setAction(async (taskArgs) => {
+    console.log(taskArgs.message);
+  });
+
+task("deploy-mainnet", "Deploys contract on a provided network")
+  .addParam("privateKey", "Please provide the private key")
+  .setAction(async ({ privateKey }) => {
+      const deployElectionContract = require("./scripts/deploy-with-param");
+      await deployElectionContract(privateKey);
+  });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
